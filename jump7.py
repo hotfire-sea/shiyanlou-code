599ed28f944158bd199ca9e238316615a8635c89
hotfire-sea <55277545@qq.com>
2019-12-19 18:56:45 +0800

    jump7

---
+++ jump7.py
@@ -0,0 +1,24 @@
+#a = 0
+#while a <= 100:
+#    a = a + 1
+#    if a % 7 == 0 or a // 10 == 7 or a % 10 == 7:
+#        continue
+#    else:
+#        print(a)
+#-------------------------------------------------------
+#for a in range(1,100):
+#    if a % 7 == 0 or a // 10 == 7 or a % 10 == 7:
+#        continue
+#    else:
+#        print(a)
+#-------------------------------------------------------
+for a in range(1,100):
+    if a % 7 == 0: 
+        continue    
+    elif a // 10 == 7: 
+        continue
+    elif a % 10 == 7:
+        continue
+    else:
+        print(a)
+
